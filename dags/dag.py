14c7ac8bbac7461ff82e7ac4015c0ac333669258
sushith <sushithks@gmail.com>
2025-02-07 22:08:57 -0500

    Initial commit

--- dags/dag.py
+++ dags/dag.py
@@ -6,12 +6,12 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime(2025, 2, 7),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=5)
 }
 
 dag = DAG(
     'amazon_books_data_transfer',
     default_args=default_args,
     description='Fetch data from Amazon and store it in Postgres',
-    schedule_interval=timedelta(days=1),
+    schedule_interval=timedelta(days=1)
 )
